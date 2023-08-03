fa5e701f4fac90f34d787dc6cfb3e224e3f01439
João Marques <jon.marques9@gmail.com>
2023-08-03 19:03:13 -0400

    questao 23

---
+++ SomaColecao.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SomaColecao {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int valor = scan.nextInt();
+        
+        if (valor > 0){
+            int soma = 0;
+            while(valor != -1){
+                soma += valor;
+                valor = scan.nextInt();
+            }
+            System.out.println(soma);
+        }
+        scan.close();
+    }   
+}
