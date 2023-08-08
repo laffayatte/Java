b4efec412dba485d29b6e55f3a8b8c2735386449
João Marques <jon.marques9@gmail.com>
2023-08-08 02:49:32 -0400

    questoes novas

--- DistanciaAviao.java
+++ DistanciaAviao.java
@@ -3,21 +3,39 @@ import java.util.Scanner;
 public class DistanciaAviao {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int matriz[][] = {{0,2,11,6,15,11,1},{2,0,7,12,4,2,15},{11,7,0,11,8,3,13},{6,12,11,0,10,2,1},{15,4,8,10,0,5,13},{11,2,3,2,5,0,14},{1,15,13,1,13,14,0}};
-        int soma=0;
+        int[][] tabela = {
+            {0, 2, 11, 6, 15, 11, 1},
+            {2, 0, 7, 12, 4, 2, 15},
+            {11, 7, 0, 11, 8, 3, 13},
+            {6, 12, 11, 0, 10, 2, 1},
+            {15, 4, 8, 10, 0, 5, 13},
+            {11, 2, 3, 2, 5, 0, 14},
+            {1, 15, 13, 1, 13, 14, 0}
+        };
 
-        int aux = scan.nextInt(), aux2;
-        while(aux!=-1){
-            
-            aux2 = scan.nextInt();
-            if(aux2==-1) break;
-            
-            soma+=matriz[((aux/111)-1)][((aux2/111)-1)];
-            aux=aux2;
-            
-        }
+        int resultado = calcularTempoViagem(tabela, scan);
 
-        System.out.printf("%d",soma);
+        System.out.println(resultado);
         scan.close();
+    }
+
+    public static int calcularTempoViagem(int[][] tabela, Scanner scan) {
+        int soma = 0;
+        int cidadeAtual = scan.nextInt();
+
+        while (true) {
+            int proximaCidade = scan.nextInt();
+
+            if (proximaCidade == -1) {
+                break;
+            }   
+
+            int cidadeOrigem = (cidadeAtual/ 111) - 1;
+            int cidadeDestino = (proximaCidade/ 111) - 1;
+
+            soma = soma + tabela[cidadeOrigem][cidadeDestino];
+            cidadeAtual = proximaCidade;
+        }
+        return soma;
     }
 }
--- OperacoesInteiros.java
+++ OperacoesInteiros.java
@@ -1,9 +1,10 @@
+/*
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class OperacoesInteiros {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         while (true) {
             ArrayList<Integer> vetor = new ArrayList<>();
             while (true) {
@@ -49,5 +50,103 @@ public class OperacoesInteiros {
             System.out.println(max);
         }
         scanner.close();
+    }
+}
+*/
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class OperacoesInteiros {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        while (true) {
+            ArrayList<Integer> vetor = lerVetor(scan);
+            
+            if (vetor.isEmpty()) {
+                break;
+            }
+            
+            int par = contarPares(vetor);
+            int impar = contarImpares(vetor);
+            int somaTotal = calcularSoma(vetor);
+            double media = calcularMedia(vetor);
+            int menor = encontrarMenor(vetor);
+            int maior = encontrarMaior(vetor);
+
+            imprimirResultados(vetor.size(), par, impar, somaTotal, media, menor, maior);
+        }
+
+        scan.close();
+    }
+    
+    public static ArrayList<Integer> lerVetor(Scanner scan) {
+        ArrayList<Integer> vetor = new ArrayList<>();
+        
+        while (true) {
+            int num = scan.nextInt();
+            if (num == -1) {
+                break;
+            }
+            vetor.add(num);
+        }
+        
+        return vetor;
+    }
+    
+    public static int contarPares(ArrayList<Integer> vetor) {
+        int count = 0;
+        for (int num : vetor) {
+            if (num % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public static int contarImpares(ArrayList<Integer> vetor) {
+        return vetor.size() - contarPares(vetor);
+    }
+    
+    public static int calcularSoma(ArrayList<Integer> vetor) {
+        int somaTotal = 0;
+        for (int num : vetor) {
+            somaTotal += num;
+        }
+        return somaTotal;
+    }
+    
+    public static double calcularMedia(ArrayList<Integer> vetor) {
+        return (double) calcularSoma(vetor) / vetor.size();
+    }
+    
+    public static int encontrarMenor(ArrayList<Integer> vetor) {
+        int menor = Integer.MAX_VALUE;
+        for (int num : vetor) {
+            if (num < menor) {
+                menor = num;
+            }
+        }
+        return menor;
+    }
+    
+    public static int encontrarMaior(ArrayList<Integer> vetor) {
+        int maior = Integer.MIN_VALUE;
+        for (int num : vetor) {
+            if (num > maior) {
+                maior = num;
+            }
+        }
+        return maior;
+    }
+    
+    public static void imprimirResultados(int quant, int par, int impar, int somaTotal, double media, int menor, int maior) {
+        System.out.println(quant);
+        System.out.println(par);
+        System.out.println(impar);
+        System.out.println(somaTotal);
+        System.out.printf("%.2f\n", media);
+        System.out.println(maior);
+        System.out.println(menor);
     }
 }
